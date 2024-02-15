0023d13feecc0e7ad84756cee5ca92f806b4fa56
gjuliao <gjuliao32@gmail.com>
2024-02-15 11:31:54 -0500

    Added hash string algorithm

---
+++ hash_string.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+class Hash
+    def param_converter
+        self.map { |i| i * '=' } * '&'
+    end
+end
+
+hash = { topic: 'baseball', team: 'astros' }
+puts hash.param_converter
+
+describe 'HTML param converter' do
+    it 'Adds an HTML param converter to hash class' do
+        hash = { topic: 'baseball', team: 'astros' }
+        expect(hash.param_converter).to eq('topic=baseball&team=astros')
+    end
+end
+
