9a11d3d2f53b71a7fe88f0826e28d58c4b06dbf3
gjuliao <gjuliao32@gmail.com>
2024-03-04 10:47:54 -0500

    Fixed array

--- multiplicationtable_generator.rb
+++ multiplicationtable_generator.rb
@@ -23,7 +23,7 @@ puts multiplication_table(5)
 describe 'Multiplication table' do
     it 'creates a hash base multiplication table' do
         hash = {
-            1 => [1,2,3,4,5,6,7,8,9,10, 12],
+            1 => [1,2,3,4,5,6,7,8,9,10, 12, 14, 16],
             2=> [2,4,6,8,10,12,14,16,18,20],
             3=> [3,6,9,12,15,18,21,24,27,30]
         }
